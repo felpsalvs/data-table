06e0d072e83a7eab3e72c33b5a8fa5e5765eee37
felpsalvs <profelipealves@outlook.com>
2023-11-13 17:13:20 -0300

    update table

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-import './style.css'
-import DataTable from 'datatables.net-dt';
+import "./style.css";
+import DataTable from "datatables.net-dt";
+import languagePTBR from "datatables.net-plugins/i18n/pt-BR.mjs";
 
-// Capturar todos tr dentro do tbody
-// Retornar a quantidades de filhos tr
-// Se a quantidade for maior que 10, aplica o DataTable
 
-// criar uma condição se existe tabela na dom
-// se existir na tabela na dom, consultar se o valor do atributo data-table é igual table-filter
-// quando for table-filter executa initTableFilter(table) 
+function checkTable(table) {
+  const tableName = table.getAttribute("data-table");
 
-function checkTable(table){
-  const tableName = table.getAttribute("data-table")
-
-  if(tableName === "table-filter"){
+  if (tableName === "table-filter") {
     initTableFilter(table);
   }
 }
 
-(function() {
-const tables = document.querySelectorAll('[data-table]')
+(function () {
+  const tables = document.querySelectorAll("[data-table]");
 
-tables.forEach((table) => {
-  const tbody = table.querySelector("tbody");
-  const tr = tbody.querySelectorAll("tr");
-  const trCount = tr.length;
-  
-  if(trCount > 10){
-  new DataTable(table, {
-    responsive: true
+  tables.forEach((table) => {
+    const tbody = table.querySelector("tbody");
+    const tr = tbody.querySelectorAll("tr");
+    const trCount = tr.length;
+
+    if (trCount > 10) {
+      new DataTable(table, {
+        responsive: true,
+        scrollX: true,
+        scrollCollapse: true,
+        columns: [
+          { width: "142px" },
+          { width: "103px" },
+          { width: "86px" },
+          { width: "134px" },
+          { width: "123px" },
+        ],
+        language: {
+          ...languagePTBR,
+          search: "",
+        },
+      });
+
+      checkTable(table);
+    }
   });
-
-  checkTable(table)
-}
-});
 })();
